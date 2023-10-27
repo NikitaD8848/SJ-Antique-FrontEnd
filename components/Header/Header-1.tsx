79531d91feccf550f8eaa67f596e028134fd8b34
priyanka8848 <priyanka@8848digital.com>
2023-10-27 15:27:31 +0530

    changes in tab

--- components/Header/Header-1.tsx
+++ components/Header/Header-1.tsx
@@ -1,46 +1,43 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import styles from "../../styles/header.module.css";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faReceipt } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from '../../styles/header.module.css';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReceipt } from '@fortawesome/free-solid-svg-icons';
 const Header1 = ({ showButtons1 }: any) => {
   return (
     <div className="container-lg">
       <div className="navbar ">
         <div className={`${styles.nav}`}>
-          
-            {showButtons1 && (
-              <div>
-                <Link
-                  className="text-decoration-none"
-                  href="/readyReceiptKundanKarigar"
-                >
-                  <button className={`${styles.button}`}>
-                    <FontAwesomeIcon
-                      className="p-1"
-                      icon={faReceipt}
-                      style={{ color: "#CDAB6E", fontSize: 20 }}
-                    />
-                    Ready Receipts(Kundan Karigar)
-                  </button>
-                </Link>
-                <Link
-                  className="text-decoration-none"
-                  href="/readyReceiptsMangalsutra"
-                >
-                  <button className={`${styles.button}`}>
-                    <FontAwesomeIcon
-                      className="p-1"
-                      icon={faReceipt}
-                      style={{ color: "#CDAB6E", fontSize: 20 }}
-                    />
-                    Ready Receipts(Mangalsutra Karigar)
-                  </button>
-                </Link>
-              </div>
-            )}
-            </div>
+          <div>
+            <Link
+              className="text-decoration-none"
+              href="/readyReceiptKundanKarigar"
+            >
+              <button className={`${styles.button}`}>
+                <FontAwesomeIcon
+                  className="p-1"
+                  icon={faReceipt}
+                  style={{ color: '#CDAB6E', fontSize: 20 }}
+                />
+                Ready Receipts(Kundan Karigar)
+              </button>
+            </Link>
+            <Link
+              className="text-decoration-none"
+              href="/readyReceiptsMangalsutra"
+            >
+              <button className={`${styles.button}`}>
+                <FontAwesomeIcon
+                  className="p-1"
+                  icon={faReceipt}
+                  style={{ color: '#CDAB6E', fontSize: 20 }}
+                />
+                Ready Receipts(Mangalsutra Karigar)
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,53 +1,51 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import Link from "next/link";
-import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "../../services/assets/Logo.png";
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import Dropdown from "react-bootstrap/Dropdown";
-import Header1 from "../Header/Header-1";
-import Header2 from "../Header/Header-2";
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Link from 'next/link';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../../services/assets/Logo.png';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Header1 from '../Header/Header-1';
+import Header2 from '../Header/Header-2';
 
 const Navbar = () => {
-    const [showButtons1, setShowButtons1] = useState<any>(false);
-    const [showButtons2, setShowButtons2] = useState<any>(false);
+  const [showButtons1, setShowButtons1] = useState<any>(false);
+  const [showButtons2, setShowButtons2] = useState<any>(false);
 
-    return(
-
-  <div className="container-lg">
-    <nav className="navbar navbar-light bg-light p-0 ">
-      <Link className="navbar-brand" href="/master">
-        <Image src={logo} alt="logo" width={96} height={62} />
-      </Link>
-      <div >
-      <button className="btn btn-primary mx-5 "
+  return (
+    <div className="container-lg">
+      <nav className="navbar navbar-light bg-light p-0 ">
+        <Link className="navbar-brand" href="/master">
+          <Image src={logo} alt="logo" width={96} height={62} />
+        </Link>
+        <div>
+          {/* <button className="btn btn-primary mx-5 "
               onClick={()=>setShowButtons1(!showButtons1)}  
                 >
                 Ready Receipts</button>
       <button className="btn btn-primary mx-5 px-5" 
-       onClick={()=>setShowButtons2(!showButtons2)} >Sales</button>
-      </div>
-      <Dropdown>
-      <Dropdown.Toggle variant="light" id="dropdown-basic">
+       onClick={()=>setShowButtons2(!showButtons2)} >Sales</button> */}
+        </div>
+        <Dropdown>
+          <Dropdown.Toggle variant="light" id="dropdown-basic">
             <FontAwesomeIcon
               icon={faCircleUser}
               className="fa fa-user-circle "
-              style={{ color: "#CDAB6E", fontSize: 30 }}
+              style={{ color: '#CDAB6E', fontSize: 30 }}
             />
-          
-      </Dropdown.Toggle>
+          </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/logout">Logout</Dropdown.Item>
-        </Dropdown.Menu>
-    </Dropdown>
-    <Header1 showButtons1={showButtons1}/>
-    <Header2 showButtons2={showButtons2}/> 
-    </nav>
-  </div>
-    )
-    };
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/logout">Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Header1 showButtons1={showButtons1} />
+        <Header2 showButtons2={showButtons2} />
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
